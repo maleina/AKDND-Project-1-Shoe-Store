a08f6677f19b3b3095d7b5682c7155e8a25999a2
Maleina Bidek <maleina@ucla.edu>
2022-08-28 09:29:31 -0400

    feat: Add welcome fragment

---
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -0,0 +1,22 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding : FragmentWelcomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome,
+            container, false)
+        return binding.root
+    }
+}
