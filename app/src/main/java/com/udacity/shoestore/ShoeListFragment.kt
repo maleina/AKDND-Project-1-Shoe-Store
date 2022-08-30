a55c5e62ef56ea2e979fb969d1d4f88c0ed84d88
Maleina Bidek <maleina@ucla.edu>
2022-08-29 20:16:20 -0400

    feat: Add shoe list fragment

--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.models.ShoeStoreViewModel
+import kotlinx.android.synthetic.main.shoe_layout.view.*
 
 class ShoeListFragment : Fragment() {
 
-    //private lateinit var viewModel: ShoeStoreViewModel
+    private val viewModel: ShoeStoreViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,7 +24,24 @@ class ShoeListFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding : FragmentShoeListBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_shoe_list, container, false)
-        //viewModel = ViewModelProvider(this).get(ShoeStoreViewModel::class.java)
+
+        // set the view model and life cycle owner in order to use data
+        // binding to populate the shoe list
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        // Add each shoe in the shoe list to the scrollable view
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoeList ->
+            shoeList.forEach { shoe ->
+                val listItem : View = inflater.inflate(R.layout.shoe_layout, null, false)
+                listItem.name_text.text = shoe.name
+                listItem.size_text.text = shoe.size.toString()
+                listItem.company_text.text = shoe.company
+                listItem.description_text.text = shoe.description
+                binding.shoeListLayout.addView(listItem)
+            }
+        })
+
         return binding.root
     }
 }
--- app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
@@ -1,10 +1,23 @@
 package com.udacity.shoestore.models
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoeStoreViewModel : ViewModel() {
-    init {
 
+    private val _shoeList = MutableLiveData<MutableList<Shoe>>()
+    val shoeList : LiveData<MutableList<Shoe>>
+        get() = _shoeList
+
+
+    init {
+        createInitialShoeList()
+    }
+
+    private fun createInitialShoeList() {
+        _shoeList.value = mutableListOf(Shoe("Air Max Plus", 8.0, "Nike", "Men's running shoe"),
+        Shoe("Keeley Field 6 Inch Boots", 6.5, "Timberland", "Women's Casual, Leather Hiking Boot"))
     }
 
     override fun onCleared() {
