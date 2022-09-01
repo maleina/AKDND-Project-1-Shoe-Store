f050429419e83420b919d76e9057e12c4c746f3c
Maleina Bidek <maleina@ucla.edu>
2022-09-01 19:35:11 -0400

    feat: Add ability to add shoe from detail screen

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.models.ShoeStoreViewModel
 
 class ShoeDetailFragment : Fragment() {
+
+    private val viewModel: ShoeStoreViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,25 @@ class ShoeDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding : FragmentShoeDetailBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_shoe_detail, container, false)
+
+        // configure view model and life cycle owner
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        // initialize "empty" shoe values
+        binding.shoe = Shoe("",0.0,"","")
+
+        binding.cancelButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        viewModel.eventShoeAdded.observe(viewLifecycleOwner, Observer { shoeAdded ->
+            if (shoeAdded){
+                findNavController().popBackStack()
+                viewModel.onNavigateComplete()
+            }
+        })
+
         return binding.root
     }
 
--- app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/models/ShoeStoreViewModel.kt
@@ -3,6 +3,7 @@ package com.udacity.shoestore.models
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 
 class ShoeStoreViewModel : ViewModel() {
 
@@ -10,14 +11,30 @@ class ShoeStoreViewModel : ViewModel() {
     val shoeList : LiveData<MutableList<Shoe>>
         get() = _shoeList
 
+    private val _eventShoeAdded = MutableLiveData<Boolean>()
+    val eventShoeAdded : LiveData<Boolean>
+        get() = _eventShoeAdded
 
     init {
+        _eventShoeAdded.value = false
         createInitialShoeList()
     }
 
     private fun createInitialShoeList() {
         _shoeList.value = mutableListOf(Shoe("Air Max Plus", 8.0, "Nike", "Men's running shoe"),
         Shoe("Keeley Field 6 Inch Boots", 6.5, "Timberland", "Women's Casual, Leather Hiking Boot"))
+    }
+
+    fun updateShoeList(shoe : Shoe) {
+        // add shoe to shoe list
+        shoeList.value?.add(shoe)
+        // set the event to true to trigger navigation
+        _eventShoeAdded.value = true
+    }
+
+    fun onNavigateComplete() {
+        // reset the event to false after navigation is complete
+        _eventShoeAdded.value = false
     }
 
     override fun onCleared() {
